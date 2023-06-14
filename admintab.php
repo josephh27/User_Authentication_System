bd1113001e5b9c0e0f2d76ac4401ad3eeef68aa3
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-06-14 20:34:59 +0800

    added admin sidebar.

--- admintab.php
+++ admintab.php
@@ -5,67 +5,87 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles/admin-style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
     <nav>
         <ul>
             <li>
-                <a href="#"></a>
-                <img src="images/university_logo.png" alt="">
-                <span>Admin</span>
-            </li>
-            <li>
-                <a href="">
-                    <i class="fas fa-user"></i>
-                    <span class="nav-item">Student</span>
+                <a href="#" class="logo nav-item">
+                    <img src="images/university_logo.png" alt="">
+                    <span class="nav-item">Admin</span>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i class="fa-solid fa-chalkboard-user"></i>
-                    <span class="">Faculty</span>
+                <a href="#" class="nav-item">
+                    <i class="fas fa-user nav-icon"></i>
+                    <span >Student</span>
                 </a>
             </li>
             <li>
-                <a href="">
-                <i class="fa-solid fa-book-bookmark"></i>
-                    <span class="">Courses</span>
+                <a href="#" class="nav-item">
+                    <i class="fa-solid fa-chalkboard-user nav-icon"></i>
+                    <span>Faculty</span>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i class="fa-solid fa-folder-open"></i>
-                    <span class="">Section</span>
+                <a href="#" class="nav-item">
+                <i class="fa-solid fa-book-bookmark nav-icon"></i>
+                    <span>Courses</span>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i class="fa-solid fa-calendar-days"></i>
-                    <span class="">Schedule</span>
+                <a href="#" class="nav-item">
+                    <i class="fa-solid fa-folder-open nav-icon"></i>
+                    <span>Section</span>
                 </a>
             </li>
             <li>
-                <a href="">
-                <i class="fa-solid fa-users-gear"></i>
-                    <span class="">Accounts</span>
+                <a href="#" class="nav-item">
+                    <i class="fa-solid fa-calendar-days nav-icon"></i>
+                    <span>Schedule</span>
                 </a>
             </li>
             <li>
-                <a href="">
-                    <i class="fa-solid fa-sliders"></i>
-                    <span class="">Settings</span>
+                <a href="#" class="nav-item">
+                <i class="fa-solid fa-users-gear nav-icon"></i>
+                    <span>Accounts</span>
                 </a>
             </li>
-            
-            
-            
-            
+            <li>
+                <a href="#" class="nav-item">
+                    <i class="fa-solid fa-sliders nav-icon"></i>
+                    <span>Settings</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="nav-item">
+                    <i class="fa-solid fa-right-from-bracket nav-icon"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
         </ul>
     </nav>
     <div class="admin-wrapper">
+        <div class="header">
+            <div class="searchBox">
+                <input class="searchInput" type="text" name="" placeholder="Search for Student ID">
+                <button class="searchButton" href="">
+                    <i class="material-icons">
+                        search
+                    </i>
+                </button>
+            </div>
+            <h1></h1>
+            <div>
 
+            </div>
+        </div>
+        <div class="tab-content">
+
+        </div>
     </div>
 </body>
 </html>
